b53bbe8c0f97d7e8e934a684523fd848c2dae8fd
Anders <anders@pecon.no>
2022-01-07 08:30:42 +0000

    Fixed asset link and profile link in dropdown menu

--- resources/views/includes/component/header-dropdown-profile.blade.php
+++ resources/views/includes/component/header-dropdown-profile.blade.php
@@ -1,11 +1,11 @@
 <div class="dropdown-menu dropdown-menu-end me-1">
-	<a href="javascript:;" class="dropdown-item">Edit Profile</a>
-	<a href="javascript:;" class="dropdown-item d-flex align-items-center">
-		Inbox
-		<span class="badge bg-danger rounded-pill ms-auto pb-4px">2</span>
-	</a>
-	<a href="javascript:;" class="dropdown-item">Calendar</a>
-	<a href="javascript:;" class="dropdown-item">Setting</a>
+	<a href="{{ route('profile.show', auth()->user()) }}" class="dropdown-item">View Profile</a>
+{{--	<a href="javascript:;" class="dropdown-item d-flex align-items-center">--}}
+{{--		Inbox--}}
+{{--		<span class="badge bg-danger rounded-pill ms-auto pb-4px">2</span>--}}
+{{--	</a>--}}
+{{--	<a href="javascript:;" class="dropdown-item">Calendar</a>--}}
+	<a href="javascript:;" class="dropdown-item">Settings</a>
 	<div class="dropdown-divider"></div>
     <form method="POST" action="{{ route('logout') }}">
         @csrf
